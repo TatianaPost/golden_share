3608c596eaa1b3c15a9df5a0f3c864c050693187
almost-no-one <yurasuao9@gmail.com>
2017-08-19 12:52:31 +0100

    valerii say good

--- golden/template-recruting.php
+++ golden/template-recruting.php
@@ -4,48 +4,56 @@
  */
 get_header(); ?>
 
-<div class="recruitment_header" style="background-image: url('<? echo get_the_post_thumbnail_url() ?>');">
+<?php if(get_the_post_thumbnail_url()) { ?>
+
+<div class="recruitment_header" style="background-image: url('<? echo get_the_post_thumbnail_url(); ?>'">
+
+  <? } else {?>
+  <div class="recruitment_header" style="background-image: url('<? echo get_template_directory_uri(); ?>/img/RECRUITMENT_top_bg.png'">
+    <? } ?>
     <article class="content">
-        <h1> <?php if(ale_get_meta('page_des')) echo ale_get_meta('page_des'); ?>
+      <h1> <?php if(ale_get_meta('page_des')) echo ale_get_meta('page_des'); else echo "100% ДОВОЛЬНЫХ КЛИЕНТОВ, <br>
+        БЛАГОДАРЯ ВЫСОКОЭФФЕКТИВНОЙ АВТОРСКОЙ <br>
+        МОДЕЛИ ПОДБОРА ПЕРСОНАЛА GOLDEN SHARE" ?>
         
-        </h1>
-        <button class="btn_consultation">Get consulting</button>
+      </h1>
+      <button class="btn_consultation">Get consulting</button>
     </article>
-</div>
-<div class="content_wrapper">
-  <div class="content">
-    <div class="sidebar">
+  </div>
+  <div class="content_wrapper">
+    <div class="content">
+      <div class="sidebar">
         <?php
         wp_nav_menu( array(
-            'theme_location' => 'header_menu', 
-            'container'       => 'nav',  
-            'container_id'    => 'recruting_menu',
-            'menu_class'      => '', 
-            'menu_id'         => '',
-            'walker'        => new Aletheme_Nav_Walker(),
-            'menu'            => '', 
-            ) );
-            ?>
+          'theme_location' => 'header_menu', 
+          'container'       => 'nav',  
+          'container_id'    => 'recruting_menu',
+          'menu_class'      => '', 
+          'menu_id'         => '',
+          'walker'        => new Aletheme_Nav_Walker(),
+          'menu'            => '', 
+          ) );
+          ?>
 
-            <button id="consultation" class="btn_consultation">Get consulting</button>
+          <button id="consultation" class="btn_consultation">Get consulting</button>
         </div>
         <div class="user_content">
-            <div class="breadcrumbs_wrapper">
-               <?php  get_breadcrumbs(); ?> 
-           </div>
-           <?
-           while ( have_posts() ) : the_post();
+          <div class="breadcrumbs_wrapper">
+           <?php  get_breadcrumbs(); ?> 
+         </div>
+         <?
+         while ( have_posts() ) : the_post();
 
-           the_content();
+         the_content();
 
-           endwhile;
-           ?>
-           <div class="after_user_content_btn_wrap">
-                <button class="btn_consultation">Get consulting</button>
-           </div>
-       </div>
-   </div>  
-</div> 
-   
+         endwhile;
+         ?>
+         <div class="after_user_content_btn_wrap">
+          <button class="btn_consultation">Get consulting</button>
+        </div>
+      </div>
+    </div>  
+  </div> 
+  
 
-<?php get_footer(); ?>
+  <?php get_footer(); ?>
--- server/template-personal.php
+++ server/template-personal.php
@@ -4,34 +4,33 @@
  */
 get_header(); ?>
 
-<div class="recruitment_header" style="background-image: url('<? echo get_the_post_thumbnail_url() ?>');">
-    <article class="content">
-        <h1> <?php if(ale_get_meta('page_des')) echo ale_get_meta('page_des'); ?>
-        
-        </h1>
-        <button class="btn_consultation">Get consulting</button>
-    </article>
-</div>
-<div class="content_wrapper">
-  <div class="content">
+<?php if(get_the_post_thumbnail_url()) { ?>
+<div class="recruitment_header services_top" style="background-image: url('<? echo get_the_post_thumbnail_url() ?>');">
+<? } else {?> 
+  <div class="recruitment_header services_top" style="background-image: url('<? echo get_template_directory_uri(); ?>/img/services_top.png'">
+    <? } ?> 
     
-        <div class="user_content">
-            <div class="breadcrumbs_wrapper">
-               <?php  get_breadcrumbs(); ?> 
-           </div>
-           <?
-           while ( have_posts() ) : the_post();
+  </div>
+  <div class="content_wrapper">
+    <div class="content">
 
-           the_content();
-
-           endwhile;
-           ?>
-           <div class="after_user_content_btn_wrap">
-                <button class="btn_consultation">Get consulting</button>
-           </div>
+      <div class="user_content">
+        <div class="breadcrumbs_wrapper">
+         <?php  get_breadcrumbs(); ?> 
        </div>
-   </div>  
+       <?
+       while ( have_posts() ) : the_post();
+
+       the_content();
+
+       endwhile;
+       ?>
+       <div class="after_user_content_btn_wrap">
+        <button class="btn_consultation">Get consulting</button>
+      </div>
+    </div>
+  </div>  
 </div> 
- 
+
 
 <?php get_footer(); ?>
